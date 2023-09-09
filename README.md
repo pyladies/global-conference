0a1d42446db31235fff4b0f6d564ee22da2cfe4b
cmaureir <cmaureir@users.noreply.github.com>
2023-09-09 11:12:59 +0000

    deploy: 8fd715c6d8def7ee7c32bfd10ecb8c5e463a9411

---
+++ README.md
@@ -0,0 +1,8 @@
+# PyLadiesCon Web
+
+## Generation
+
+Go to the `generate_site/` directory,
+install the requirements from `requirements.txt`,
+and that wil generate the `index.html` and `about.html`
+files for the web.
---
+++ generate_site/generate.py
@@ -0,0 +1,34 @@
+import sys
+import requests
+import pandas as pd
+
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader, Template
+
+
+# Files
+ORG = "organization.csv"
+
+# It is a dev run?
+dev = True if len(sys.argv) > 1 else False
+
+# Organization info
+df_org = pd.read_csv("organizers.csv")[["Name", "Role", "Biography"]]
+df_org.fillna("", inplace=True)
+# TODO: Download images? 'Headshot image' is the column name
+
+##### Template configuration
+conf = {
+    "ORG": df_org.to_dict("records"),
+}
+
+templates = ["about", "index"]
+environment = Environment(loader=FileSystemLoader("templates/"))
+
+for _template in templates:
+    _t = environment.from_string(open(f"templates/{_template}-base.html").read())
+
+    # Write final HTML
+    with open(Path("..") / f"{_template}.html", "w") as f:
+        f.write(_t.render(conf))
