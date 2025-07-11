4d689aa01436058c1bbb139f056c4713bd30afcd
mariatta <mariatta.wijaya@gmail.com>
2025-07-11 11:07:54 -0700

    Fix the build

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,24 +1,3 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
-
-export async function GET(context) {
-  return rss({
-    // `<title>` field in output xml
-    title: 'PyLadiesCon News and Announcements',
-    // `<description>` field in output xml
-    description: 'News and announcements from PyLadiesCon, a Global PyLadies Conference.',
-    // Pull in your project "site" from the endpoint context
-    // https://docs.astro.build/en/reference/api-reference/#site
-    site: context.site,
-    // Array of `<item>`s in output xml
-    // See "Generating items" section for examples using content collections and glob imports
-    items: await pagesGlobToRssItems(
-      import.meta.glob('../posts/*.{md,mdx}'),
-    ),
-    // (optional) inject custom xml
-    customData: `<language>en-us</language>`,
-  });
-}
-
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
