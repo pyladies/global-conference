4e8dda68f0d5e375341179cf6997cb01885a7a12
egeakman <me@egeakman.dev>
2025-10-22 21:05:18 -0400

    static it is

--- astro.config.mjs
+++ astro.config.mjs
@@ -49,7 +49,12 @@ export default defineConfig({
     highlighter: getHighlighter,
   },
   redirects: {
-    "/docs/committee_[slug]": "/docs/volunteers/committee_[slug]",
+    "/docs/committee_communications": "/docs/volunteers/committee_communications",
+    "/docs/committee_design": "/docs/volunteers/committee_design",
+    "/docs/committee_finance": "/docs/volunteers/committee_finance",
+    "/docs/committee_infra": "/docs/volunteers/committee_infra",
+    "/docs/committee_program": "/docs/volunteers/committee_program",
+    "/docs/committee_volunteers": "/docs/volunteers/committee_volunteers",
     "/docs/roles_and_responsibilities": "/docs/volunteers/roles_and_responsibilities",
     "/docs/speaker_guide": "/docs/speakers/speaker_guide",
   }
